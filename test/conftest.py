38e93d0e9067677f8605437bdec82e5f0415f6b1
Andrew Sanchez <inbox.asanchez@gmail.com>
2017-10-21 12:22:18 +0200

    Use betamax

--- test/conftest.py
+++ test/conftest.py
@@ -1,16 +1,23 @@
 import pytest
+import betamax
 
 from memworth.record import Record
 from memworth.scraper import Word
 
+with betamax.Betamax.configure() as config:
+    config.cassette_library_dir = 'test/cassettes'
+
 
 @pytest.fixture(scope="module",
-                params=[("gezellig", "resources/gezellig.html"),
-                        ("nonword", None)])
+                params=["gezellig", "nonword", "verhief", "overeenkomt"])
 def words(request):
-    word = Word(request.param[0], request.param[1])
-    word.get_word_data()
-    yield word
+    word = request.param
+    w = Word(word)
+    recorder = betamax.Betamax(w.session)
+    with recorder.use_cassette(word):
+        w.get_response()
+    w.get_word_data()
+    yield w
 
 
 @pytest.fixture(scope="module",
@@ -20,6 +27,9 @@ def conjugated(request):
     conjugated = request.param[0]
     infinitive = request.param[1]
     w = Word(conjugated)
+    recorder = betamax.Betamax(w.session)
+    with recorder.use_cassette(conjugated):
+        w.get_response()
     w.get_word_data()
     yield infinitive, w
 
